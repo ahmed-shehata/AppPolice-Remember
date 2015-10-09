615d289c2c37b215d082fdb0da215099e680e5c8
ahmed-shehata <ahmed.shehata@espace.com.eg>
2015-10-10 00:34:53 +0200

    Added binary

---
+++ AppPolice.app/Contents/Frameworks/ChromeMenu.framework/Versions/A/Headers/ChromeMenu.h
@@ -0,0 +1,11 @@
+//
+//  ChromMenu.h
+//  ChromeMenu
+//
+//  Created by Maksym on 7/4/13.
+//  Copyright (c) 2013 Maksym Stefanchuk. All rights reserved.
+//
+
+
+#import "CMMenu.h"
+#import "CMMenuItem.h"
